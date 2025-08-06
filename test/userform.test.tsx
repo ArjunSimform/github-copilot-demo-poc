1ad06fed184e4bbedff32cbeb99f09ec871d4002
ArjunSimform <arjunsingh.gurjar@simformsolutions.com>
2025-08-06 16:01:55 +0530

    remove : remove comment

--- test/userform.test.tsx
+++ test/userform.test.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
-import { render } from "vitest-browser-react";
-import { describe, it, expect } from "vitest";
-import App from "../src/App";
-describe("App", () => {
-  it("renders User Management System text", async () => {
+import { render } from 'vitest-browser-react';
+import { describe, it, expect } from 'vitest';
+import App from '../src/App';
+describe('App', () => {
+  it('renders User Management System text', async () => {
     const screen = render(<App />);
     await expect
       .element(screen.getByText(/User Management System/i))
@@ -13,14 +13,3 @@ describe("App", () => {
     expect(screen.getByText(/User Management System/i)).toBeInTheDocument();
   });
 });
-
-// import { render } from 'vitest-browser-react'
-// import { expect, test } from 'vitest'
-
-// test('counter button increments the count', async () => {
-//   const screen = render(<Component count={1} />)
-
-//   await screen.getByRole('button', { name: 'Increment' }).click()
-
-//   await expect.element(screen.getByText('Count is 2')).toBeVisible()
-// })
